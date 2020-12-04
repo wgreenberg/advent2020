3b267be4687fd5b1b6f9898754cef6fb2b7468df
Will Greenberg <willg@eff.org>
2020-12-04 02:05:24 -0800

    day 4

---
+++ day4/main.rb
@@ -0,0 +1,46 @@
+def has_valid_fields(passport)
+  ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].all? do |req_field|
+    passport.map(&:first).include? req_field
+  end
+end
+
+def into_passports(input)
+  input.split("\n\n")
+    .map(&:split)
+    .map { |passport| passport.map { |parts| parts.split(":") }}
+end
+
+puts into_passports(File.open("day4/input").read)
+  .count { |passport| has_valid_fields(passport) }
+
+def valid_height(value)
+  return value.to_i.between?(150, 193) if value.end_with? "cm"
+  return value.to_i.between?(59, 76) if value.end_with? "in"
+  false
+end
+
+def validate_passport_fields(passport)
+  passport.all? do |field, value|
+    next value.to_i.between?(1920, 2002) if field == "byr"
+    next value.to_i.between?(2010, 2020) if field == "iyr"
+    next value.to_i.between?(2020, 2030) if field == "eyr"
+    next valid_height(value) if field == "hgt"
+    next /#[a-f0-9]{6}/.match? value if field == "hcl"
+    next ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include? value if field == "ecl"
+    next value.length == 9 && value.to_i != 0 if field == "pid"
+    next true if field == "cid"
+    false
+  end
+end
+
+raise "failed invalid" unless into_passports(File.open("day4/invalid").read)
+  .filter { |passport| has_valid_fields(passport) }
+  .count { |passport| validate_passport_fields(passport) } == 0
+
+raise "failed valid" unless into_passports(File.open("day4/valid").read)
+  .filter { |passport| has_valid_fields(passport) }
+  .count { |passport| validate_passport_fields(passport) } == 4
+
+puts into_passports(File.open("day4/input").read)
+  .filter { |passport| has_valid_fields(passport) }
+  .count { |passport| validate_passport_fields(passport) }
