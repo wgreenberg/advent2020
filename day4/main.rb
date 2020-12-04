9c61ea2731680cb97ecba4a2edbd38afea738dd9
Will Greenberg <willg@eff.org>
2020-12-04 02:46:54 -0800

    idk if this is good but slightly cleaner code

--- day4/main.rb
+++ day4/main.rb
@@ -1,41 +1,49 @@
-def has_valid_fields(passport)
-  ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].all? do |req_field|
-    passport.map(&:first).include? req_field
+class Array
+  def has_required_fields?
+    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].all? do |req_field|
+      self.map(&:first).include? req_field
+    end
+  end
+
+  def has_valid_fields?
+    self.all? do |field, value|
+      next value.to_i.between?(1920, 2002) if field == "byr"
+      next value.to_i.between?(2010, 2020) if field == "iyr"
+      next value.to_i.between?(2020, 2030) if field == "eyr"
+      next value.to_i.between?(150, 193) if field == "hgt" && value.end_with?("cm")
+      next value.to_i.between?(59, 76) if field == "hgt" && value.end_with?("in")
+      next /#[a-f0-9]{6}/.match? value if field == "hcl"
+      next ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include? value if field == "ecl"
+      next value.length == 9 && value.to_i != 0 if field == "pid"
+      next true if field == "cid"
+      false
+    end
   end
 end
 
-def into_passports(input)
-  input.split("\n\n")
-    .map(&:split)
-    .map { |passport| passport.map { |parts| parts.split(":") }}
-end
-
-puts into_passports(File.open("day4/input").read)
-  .count { |passport| has_valid_fields(passport) }
-
-def validate_passport_fields(passport)
-  passport.all? do |field, value|
-    next value.to_i.between?(1920, 2002) if field == "byr"
-    next value.to_i.between?(2010, 2020) if field == "iyr"
-    next value.to_i.between?(2020, 2030) if field == "eyr"
-    next value.to_i.between?(150, 193) if field == "hgt" && value.end_with?("cm")
-    next value.to_i.between?(59, 76) if field == "hgt" && value.end_with?("in")
-    next /#[a-f0-9]{6}/.match? value if field == "hcl"
-    next ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include? value if field == "ecl"
-    next value.length == 9 && value.to_i != 0 if field == "pid"
-    next true if field == "cid"
-    false
+class String
+  def into_passports
+    self.split("\n\n")
+      .map(&:split)
+      .map { |passport| passport.map { |parts| parts.split(":") }}
   end
 end
 
-raise "failed invalid" unless into_passports(File.open("day4/invalid").read)
-  .filter { |passport| has_valid_fields(passport) }
-  .count { |passport| validate_passport_fields(passport) } == 0
+puts File.open("day4/input").read
+  .into_passports
+  .count(&:has_required_fields?)
 
-raise "failed valid" unless into_passports(File.open("day4/valid").read)
-  .filter { |passport| has_valid_fields(passport) }
-  .count { |passport| validate_passport_fields(passport) } == 4
+raise "failed invalid" unless File.open("day4/invalid").read
+  .into_passports
+  .filter(&:has_required_fields?)
+  .count(&:has_valid_fields?) == 0
 
-puts into_passports(File.open("day4/input").read)
-  .filter { |passport| has_valid_fields(passport) }
-  .count { |passport| validate_passport_fields(passport) }
+raise "failed valid" unless File.open("day4/valid").read
+  .into_passports
+  .filter(&:has_required_fields?)
+  .count(&:has_valid_fields?) == 4
+
+puts File.open("day4/input").read
+  .into_passports
+  .filter(&:has_required_fields?)
+  .count(&:has_valid_fields?)
