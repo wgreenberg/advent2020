3a5448993ee8fdc0c22e579b5060f08f7af5d2eb
Will Greenberg <willg@eff.org>
2020-12-07 00:45:29 -0800

    some cleanups for readability

--- day7/main.rb
+++ day7/main.rb
@@ -1,21 +1,25 @@
 rules = File.open("day7/input")
   .readlines(chomp: true)
   .map { |line| line.gsub(/\sbags?/, "").split(" contain ") }
-  .map { |lhs, rhs| [lhs, rhs.delete_suffix(".").split(", ").filter { |v| v != "no other" }] }
-  .map { |lhs, rhs| [lhs, rhs.map { |rule| [rule.to_i, rule.split.drop(1).join(" ")] }]}
-  .to_h
+  # turns rules like "1 x, 7 y" into [[1, "x"], [7, "y"]]
+  .map do |bag, rhs|
+    rule = rhs.delete_suffix(".").split(", ")
+      .filter { |v| v != "no other" }
+      .map { |rule| [rule.to_i, rule.split.drop(1).join(" ")] }
+    [bag, rule]
+  end.to_h
 
-def reverse_search(data, color)
-  data.select { |parent, children| children.any? { |n, c| c == color }}.keys
-    .map { |parent| reverse_search(data, parent).push(parent) }
+def reverse_search(data, search_color)
+  data.select { |parent, children| children.any? { |n, color| color == search_color }}
+    .keys.map { |parent| reverse_search(data, parent).push(parent) }
     .flatten
     .uniq
 end
 
 puts reverse_search(rules, "shiny gold").length
 
-def total_bags(data, color)
-  data[color].map { |n, c| n * (1 + total_bags(data, c)) }.sum
+def total_bags(data, search_color)
+  data[search_color].map { |n, color| n * (1 + total_bags(data, color)) }.sum
 end
 
 puts total_bags(rules, "shiny gold")
